d3637e4c8b987e08be3a9659964b7956a3b31d88
Perry Naseck <DaAwesomeP@users.noreply.github.com>
2021-06-16 11:55:26 -0400

    Rework Python build process, rework Python CI, build aarch64 Python

--- wrappers/python/find_librs_version.py
+++ wrappers/python/find_librs_version.py
@@ -3,35 +3,39 @@ import re
 import sys
 import os
 
-librs_version = ''
-if len(sys.argv) < 3:
-    print("Error! Usage: find_librs_version.py <absolute_path_to_librealsense> <output_dir>")
-    exit(1)
+def get_version(librealsense_dir, output_dir=None, printOutput=False):
+    librs_version = ''
+    rs_h_path = os.path.join(librealsense_dir, 'include/librealsense2/rs.h')
+    if printOutput:
+        print("Extracting version from: ", rs_h_path)
+    with io.open(rs_h_path, 'r') as f:
+        file_content = f.read()
+        major = re.search(r"#define\s*RS2_API_MAJOR_VERSION\s*(\d+)",file_content)
+        if not major:
+            raise Exception('No major number')
+        librs_version += major.group(1)
+        librs_version += '.'
+        minor = re.search(r"#define\s*RS2_API_MINOR_VERSION\s*(\d+)",file_content)
+        if not minor:
+            raise Exception('No minor number')
+        librs_version += minor.group(1)
+        librs_version += '.'
+        patch = re.search(r"#define\s*RS2_API_PATCH_VERSION\s*(\d+)",file_content)
+        if not patch:
+            raise Exception('No patch number')
+        librs_version += patch.group(1)
 
-librealsense_dir = sys.argv[1]
-output_dir = sys.argv[2]
+        if printOutput:
+            print("Librealsense Version: ", librs_version)
+        if output_dir is not None:
+            outfile = os.path.join(output_dir, '_version.py')
+            print("Writing version to: ", outfile)
+            with open(outfile, 'w') as f:
+                f.write('__version__ = "{}"'.format(librs_version))
+        return librs_version
 
-rs_h_path = os.path.join(librealsense_dir, 'include/librealsense2/rs.h')
-print("Extracting version from: ", rs_h_path)
-with io.open(rs_h_path, 'r') as f:
-    file_content = f.read()
-    major = re.search(r"#define\s*RS2_API_MAJOR_VERSION\s*(\d+)",file_content)
-    if not major:
-        raise Exception('No major number')
-    librs_version += major.group(1)
-    librs_version += '.'
-    minor = re.search(r"#define\s*RS2_API_MINOR_VERSION\s*(\d+)",file_content)
-    if not minor:
-        raise Exception('No minor number')
-    librs_version += minor.group(1)
-    librs_version += '.'
-    patch = re.search(r"#define\s*RS2_API_PATCH_VERSION\s*(\d+)",file_content)
-    if not patch:
-        raise Exception('No patch number')
-    librs_version += patch.group(1)
-
-    print("Librealsense Version: ", librs_version)
-    outfile = os.path.join(output_dir, '_version.py')
-    print("Writing version to: ", outfile)
-    with open(outfile, 'w') as f:
-        f.write('__version__ = "{}"'.format(librs_version))
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print("Error! Usage: find_librs_version.py <absolute_path_to_librealsense> <output_dir>")
+        exit(1)
+    get_version(sys.argv[1], sys.argv[2], write_file=True, printOutput=True)
---
+++ wrappers/python/install_build_deps.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ -n "$(which apt-get)" ]; then
+  apt-get install -y build-essential cmake libssl-dev libusb-1.0-0-dev pkg-config libgtk-3-dev
+elif [ -n "$(which yum)" ]; then
+  yum install -y openssl-devel libusb-devel gtk3-devel
+fi
