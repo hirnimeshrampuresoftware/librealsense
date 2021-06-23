5d8d9701dfe0737815c7b8394bbd14277be72f76
Perry Naseck <DaAwesomeP@users.noreply.github.com>
2021-06-23 09:45:19 -0400

    move install script, manylinux2014, shim only cibuildwheel

--- scripts/github_action_install_build_deps_linux.sh
+++ scripts/github_action_install_build_deps_linux.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [ -n "$(which apt-get)" ]; then
+  # manylinux and GitHub runners using Debian
   apt-get install -y build-essential cmake libssl-dev libusb-1.0-0-dev pkg-config libgtk-3-dev
 elif [ -n "$(which yum)" ]; then
+  # manylinux runners using CentOS
   yum install -y openssl-devel libusb1-devel gtk3-devel kernel-devel
 fi
